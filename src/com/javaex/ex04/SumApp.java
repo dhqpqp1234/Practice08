382d74b52612be475c170c70b8fc5fad0f31a9ae
dhqpqp1234 <dhqpqp1234@naver.com>
2022-05-01 23:20:00 +0900

    2022-04-29과제

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class SumApp {
 
 	public static void main(String[] args) {
-		
 		int sum = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 공백으로 구분해서 입력하세요");
 		String numLine = sc.nextLine();
 	
 		//코드를 작성하세요
-		int num = Integer.parseInt(numLine);
-		sum = num + num;
+		
+		String[] nArray = numLine.split(" ");
 		
 		
+		 
 		System.out.println("합:" + sum);
 		
 		sc.close();
--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -1,8 +1,42 @@
 package com.javaex.ex05;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
+    int input1 = sc.nextInt();
+    String input3 = sc.next();
+    int input2 = sc.nextInt();
+    
+    
+    
+    if(input3.equals("+")) {
+    	Add a = new Add();
+    	a.setValue(input1, input2);
+    	System.out.println(a.calculate());
+    }else if(input3.equals("-")) {
+    	Sub s = new Sub();
+    	s.setValue(input1, input2);
+    	System.out.println(s.calculate());
+    }else if(input3.equals("*")) {
+    	Mul m = new Mul();
+    	m.setValue(input1, input2);
+    	System.out.println(m.calculate());
+    }else if(input3.equals("/")) {
+    	Div d = new Div();
+    	d.setValue(input1, input2);
+    	System.out.println(d.calculate());
+    }else if(input3.equals("%")) {
+    	System.out.println("알 수 없는 연산입니다.");
+    }else if(input3.equals("/q")) {
+    	System.out.println("종료합니다.");
+    }
+    
+    
+    sc.close();
+    	
     }
 }
